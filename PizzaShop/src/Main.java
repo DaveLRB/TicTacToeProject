c041d61ed14c82889477df5e0cddb87924a738ae
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-27 12:45:52 +0000

    used ENUMs

--- PizzaShop/src/Main.java
+++ PizzaShop/src/Main.java
@@ -2,5 +2,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        StatesOfPizza hawaiana = StatesOfPizza.ORDERED;
+        hawaiana.printTime();
+        StatesOfPizza diavola = StatesOfPizza.DELIVERED;
+        diavola.printTime();
+        StatesOfPizza mare = StatesOfPizza.READY;
+        mare.printTime();
+
+
+
     }
 }
--- PizzaShop/src/StatesOfPizza.java
+++ PizzaShop/src/StatesOfPizza.java
@@ -1,18 +1,53 @@
 //order takes 5 min
 //ready takes 2 min
-//delivered takes 8 min
+//delivered takes 0 min
 // isDelivered();
 // isOrdered();
 //isReady();
 //printTime();
 
 public enum StatesOfPizza {
+    ORDERED(5) {
+        @Override
+        public boolean isOrdered() {
+            return true;
 
-    ORDERED,
-    READY,
-    DELIVERED;
+        }
+    },
+    READY(2) {
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+    },
+    DELIVERED(0){
+        @Override
+        public boolean isDelivered() {
+            return true;
+        }
+    };
 
-    StatesOfPizza() {
+    StatesOfPizza(int time) {
+        this.time=time;
 
     }
+
+    public boolean isDelivered(){
+        return false;
+    }
+
+    public boolean isOrdered(){
+        return false;
+    }
+
+    public boolean isReady(){
+        return false;
+    }
+
+    public void printTime(){
+        System.out.println(time);
+    }
+
+    private int time;
+
 }
