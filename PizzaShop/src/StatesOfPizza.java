fda34ef45379b1458132acf49ceead51953e9690
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-27 12:19:21 +0000

    first commit

---
+++ PizzaShop/src/StatesOfPizza.java
@@ -0,0 +1,18 @@
+//order takes 5 min
+//ready takes 2 min
+//delivered takes 8 min
+// isDelivered();
+// isOrdered();
+//isReady();
+//printTime();
+
+public enum StatesOfPizza {
+
+    ORDERED,
+    READY,
+    DELIVERED;
+
+    StatesOfPizza() {
+
+    }
+}
